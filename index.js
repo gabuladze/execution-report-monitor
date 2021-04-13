1f4d5f502d32f82712fd95e7421efc099ced7ffc
Giorgi Abuladze <itisabu@gmail.com>
2021-04-13 16:38:07 +0400

    Added start function to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Started')
+const ws = require('./ws')
+
+async function start () {
+  try {
+    console.log('Starting...')
+    await ws.init()
+  } catch (error) {
+    console.log('An error has occured:', error.stack)
+  }
+}
+
+start()
