2744cbfc6a367eabf463be96a512d263eb6c0059
Giorgi Abuladze <itisabu@gmail.com>
2021-04-13 15:05:42 +0400

    Implemented getSignature, fixed getHeaders, pass tests

--- api/index.js
+++ api/index.js
@@ -1,20 +1,33 @@
 const crypto = require('crypto')
 
-const { SECRET } = require('../config')
+const { API_KEY, SECRET } = require('../config')
+
+/**
+ * Returns the sha256 HMAC hash for paramString & secret combo
+ * @param {string} paramString - Query string of request parameters
+ * @returns {Object} containing signature param that is a string
+ */
+const getSignature = (paramString) => {
+  let signature = crypto.createHmac('sha256', SECRET)
+  if (paramString) {
+    signature = signature.update(paramString)
+  }
+  signature = signature.digest('hex')
+
+  return { signature }
+}
 
 /**
  * Returns object request headers
- * @param {string} paramString - Query string of request parameters
  * @returns {Object} Object with Content-Type & X-MBX-APIKEY headers.
  *  (can pass to axios options directly)
  */
-const getHeaders = (paramString) => {
-  const signature = crypto.createHmac('sha512', SECRET).update(paramString).digest('hex')
+const getHeaders = () => {
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'X-MBX-APIKEY': signature
+    'X-MBX-APIKEY': API_KEY
   }
   return headers
 }
 
-module.exports = { getHeaders }
+module.exports = { getSignature, getHeaders }
