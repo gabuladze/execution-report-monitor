2c2cd87692f298de70b6c2d278b1d006aef87fc4
Giorgi Abuladze <itisabu@gmail.com>
2021-04-14 10:30:41 +0400

    Added test for onError event handler

--- tests/eventHandlers.test.js
+++ tests/eventHandlers.test.js
@@ -13,4 +13,18 @@ describe('Test API functions', () => {
     // Assert
     expect(consoleSpy).toHaveBeenCalledWith(expectedMessage)
   })
+
+  test('onError must log correct message', async () => {
+    // Arrange
+    expect.assertions(1)
+    const consoleSpy = jest.spyOn(console, 'log')
+    const expectedMessage = 'Received error event!'
+    const testError = new Error('ERR_TEST_ERROR')
+
+    // Act
+    eventHandlers.onError(testError)
+
+    // Assert
+    expect(consoleSpy).toHaveBeenCalledWith(expectedMessage)
+  })
 })
