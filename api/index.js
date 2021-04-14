cc9806e131c0c16d371e04ee03970fdb55443252
Giorgi Abuladze <itisabu@gmail.com>
2021-04-14 11:51:26 +0400

    Implemented getAccountInfo, fixed getSignature

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const axios = require('axios')
 const crypto = require('crypto')
+const querystring = require('querystring')
 
 const { API_URL, API_KEY, SECRET } = require('../config')
 
 /**
  * Returns the sha256 HMAC hash for paramString & secret combo
  * @param {string} paramString - Query string of request parameters
- * @returns {Object} containing signature param that is a string
+ * @returns {string} sha256 HMAC signature hex string
  */
 const getSignature = (paramString) => {
   let signature = crypto.createHmac('sha256', SECRET)
@@ -15,7 +16,7 @@ const getSignature = (paramString) => {
   }
   signature = signature.digest('hex')
 
-  return { signature }
+  return signature
 }
 
 /**
@@ -53,4 +54,30 @@ const createListenKey = async () => {
   }
 }
 
-module.exports = { getSignature, getHeaders, createListenKey }
+/**
+ * Returns account info from `GET /api/v3/account`
+ * @async
+ */
+const getAccountInfo = async () => {
+  try {
+    const url = `${API_URL}/v3/account`
+    const headers = getHeaders()
+    const params = { timestamp: Date.now() }
+    const paramString = querystring.stringify(params)
+
+    // Add HMAC signature to param string
+    params.signature = getSignature(paramString)
+
+    const response = await axios.get(url, { params, headers })
+
+    return response.data
+  } catch (error) {
+    console.log('Error when fetching account info')
+    console.log(error.stack)
+    if (error.response) {
+      console.log('error.response.data=', error.response.data)
+    }
+  }
+}
+
+module.exports = { getSignature, getHeaders, createListenKey, getAccountInfo }
