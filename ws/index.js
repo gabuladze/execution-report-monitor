aa0e63697d9f8a1d63d1d44730946244c47438a4
Giorgi Abuladze <itisabu@gmail.com>
2021-04-15 14:20:42 +0400

    Removed hardcoded listenkey

--- ws/index.js
+++ ws/index.js
@@ -2,15 +2,17 @@
 const WebSocket = require('ws')
 const { STREAM_WS_URL } = require('../config')
 const eventHandlers = require('./eventHandlers.js')
+const HttpClient = require('../api/httpClient.js')
+const UserDataStreamsService = require('../api/userDataStreamsService.js')
 
 let conn = null // Websocket connection instance
 
 const init = async () => {
   try {
     console.log('Initializing the connection to websocket server...')
-    // const { listenKey } = await API.createListenKey()
-    // const listenKey='mz3VxKVVj615gXgijvxm8YznAxy02LhoIRUCdnOj39c3aR8qGRHP1OtrhrgT'
-    const listenKey = 'mz3VxKVVj615gXgijvxm8YznAxy02LhoIRUCdnOj39c3aR8qGRHP1OtrhrgT'
+    const UserDataStreamsServiceInstance = new UserDataStreamsService({ HttpClient })
+    const { listenKey } = await UserDataStreamsServiceInstance.createListenKey()
+    
     console.log(`Created listen key... listenKey=${listenKey}`)
     // By convention, the stream is available at /ws/<listenKey>
     const streamUrl = `${STREAM_WS_URL}/${listenKey}`
@@ -19,10 +21,10 @@ const init = async () => {
       conn = new WebSocket(streamUrl)
       conn.on('open', eventHandlers.onOpen)
 
-      //   conn.on('ping', (data) => {
-      //     console.log('received ping')
-      //     console.log('data=', data.toJSON())
-      //   })
+      // conn.on('ping', (data) => {
+      //   console.log('received ping')
+      //   console.log('data=', data.toJSON())
+      // })
 
       //   conn.on('pong', (data) => {
       //     console.log('PONG')
