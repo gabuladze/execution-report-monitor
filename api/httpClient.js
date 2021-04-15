33b24542a948c1d8a7f3c0501462ccb8d6b0eafe
Giorgi Abuladze <itisabu@gmail.com>
2021-04-15 16:02:06 +0400

    Use default request timeout for axios instance

--- api/httpClient.js
+++ api/httpClient.js
@@ -5,7 +5,6 @@ const querystring = require('querystring')
 
 const AxiosInstance = axios.create({
   baseURL: API_URL,
-  timeout: 1000,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
     'X-MBX-APIKEY': API_KEY
