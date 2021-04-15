e0bfa35c8d79ff5d2b74a7e00bbf98f3a483d63b
Giorgi Abuladze <itisabu@gmail.com>
2021-04-15 12:58:14 +0400

    Added test for SpotAccountService.getAccountInfo

---
+++ tests/spotAccountsService.test.js
@@ -0,0 +1,42 @@
+const HttpClient = require('../api/httpClient')
+const { API_KEY, SECRET } = require('../config')
+const SpotAccountService = require('../api/spotAccountsService.js')
+
+let SpotAccountServiceInstance
+beforeAll(async () => {
+  SpotAccountServiceInstance = new SpotAccountService({ HttpClient, apiKey: API_KEY, apiSecret: SECRET })
+})
+
+describe('Test SpotAccountService', () => {
+  test('getAccountInfo must return current account info', async () => {
+    // Arrange
+    expect.assertions(14)
+
+    // Act
+    const result = await SpotAccountServiceInstance.getAccountInfo()
+
+    // Assert
+    expect(result).toHaveProperty('makerCommission')
+    expect(result).toHaveProperty('makerCommission')
+    expect(result).toHaveProperty('takerCommission')
+    expect(result).toHaveProperty('buyerCommission')
+    expect(result).toHaveProperty('sellerCommission')
+    expect(result).toHaveProperty('canTrade')
+    expect(result).toHaveProperty('canWithdraw')
+    expect(result).toHaveProperty('canDeposit')
+    expect(result).toHaveProperty('updateTime')
+    expect(result).toHaveProperty('accountType')
+    expect(result).toHaveProperty('balances')
+    expect(Array.isArray(result.balances)).toBe(true)
+    expect(Array.isArray(result.permissions)).toBe(true)
+    expect(result.balances).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          asset: expect.any(String),
+          free: expect.any(String),
+          locked: expect.any(String)
+        })
+      ])
+    )
+  })
+})
