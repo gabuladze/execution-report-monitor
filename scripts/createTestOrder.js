7823d110cd6ccb158919bb03e0feee46c8560d9b
Giorgi Abuladze <itisabu@gmail.com>
2021-04-15 19:42:41 +0400

    Added command line argument handling to createTestOrder script

--- scripts/createTestOrder.js
+++ scripts/createTestOrder.js
@@ -1,15 +1,44 @@
 const HttpClient = require('../api/httpClient')
 const SpotAccountsService = require('../api/spotAccountsService.js')
+const MarketDataService = require('../api/marketDataService.js')
 
 async function start () {
   try {
+    const args = process.argv.slice(2)
+    const symbol = args[0]
+    const side = args[1]
+    const quantity = args[2]
+
+    if (args.length === 0) throw new Error('symbol, side & quantity arguments are required!')
+    if (!symbol) throw new Error('symbol argument is required!')
+    if (!side) throw new Error('side argument is required!')
+    if (!['BUY', 'SELL'].includes(side)) throw new Error('invalid side argument value! must be: BUY or SELL')
+    if (!quantity) throw new Error('quantity argument is required!')
+    if (quantity <= 0) throw new Error('quantity must be > 0!')
+
+    const MarketDataServiceInstance = new MarketDataService({ HttpClient })
     const SpotAccountsServiceInstance = new SpotAccountsService({ HttpClient })
 
+    const exchangeInfo = await MarketDataServiceInstance.getExchangeInfo()
+
+    // Find specified symbol
+    const symbolInfo = exchangeInfo.symbols.filter(s => s.symbol === symbol)[0]
+    if (!symbolInfo) throw new Error(`${symbol} not found!`)
+
+    const { baseAsset, quoteAsset } = symbolInfo
+
+    const accountInfo = await SpotAccountsServiceInstance.getAccountInfo()
+
+    // Find balance for baseAsset if we are selling, otherwise for quoteAsset
+    const requiredAsset = side === 'SELL' ? baseAsset : quoteAsset
+    const balance = accountInfo.balances.filter(b => b.asset === requiredAsset)[0]
+    if (Number(balance.free) < quantity) throw new Error(`Insufficient funds! quantity=${quantity} balance=${balance.free}`)
+
     const testOrderParams = {
-      symbol: 'BNBBUSD',
-      side: 'SELL',
+      symbol,
+      side,
       type: 'MARKET',
-      quantity: '10',
+      quantity,
       timestamp: Date.now()
     }
 
@@ -19,7 +48,7 @@ async function start () {
     const responseData = await SpotAccountsServiceInstance.newOrder(testOrderParams)
     console.log('Response data: ', responseData)
   } catch (error) {
-    console.log('Error:', error.stack)
+    console.log(error.stack)
   }
 }
 
